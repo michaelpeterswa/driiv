21684b61f7ba5f8cc298dbdaaf58139bd4521cc0
Michael Peters <michaelpeterswa@icloud.com>
2020-07-14 19:40:14 -0700

    feat: added full program functionality

--- src/server.js
+++ src/server.js
@@ -15,28 +15,25 @@ const app_port = 6981;
 const app = express();
 app.set("view engine", "ejs");
 
+var geometry = "";
+var location = "";
+var data_size = "";
+var time = "";
+
 const file = fs.readFileSync("data/secrets.yml", "utf8");
 yamlfile = YAML.parse(file);
 
 apikey = yamlfile.secrets.apikey;
 appid = yamlfile.secrets.appid;
 
-// var request = {
-//   departure_searches: [
-//     {
-//       id: "first_location",
-//       coords: latLng,
-//       transportation: {
-//         type: "car",
-//       },
-
-//       departure_time: departureTime,
-//       travel_time: travelTime,
-//     },
-//   ],
-
-//   arrival_searches: [],
-// };
+var post_info = {
+  address: "",
+  coordinates: {},
+  terabytes: 0,
+  minutes: 0,
+  int_speed: 0,
+  gt_speed: 0,
+};
 
 //////////////////////////////////
 
@@ -48,9 +45,9 @@ app.use(
 );
 app.use(express.static("public"));
 
-function getTimeMapAPI(lat_long) {
+function getTimeMapAPI(lat_long, res2) {
   var departureTime = new Date().toJSON();
-  var travelTime = 60 * 60;
+  var travelTime = post_info.minutes * 60;
 
   var request = {
     departure_searches: [
@@ -76,11 +73,12 @@ function getTimeMapAPI(lat_long) {
     .set("Content-Type", "application/json; charset=UTF-8")
     .send(JSON.stringify(request))
     .end(function (err, res) {
-      console.log(res.text);
+      geometry = JSON.parse(res.text);
+      res2.redirect("/result");
     });
 }
 
-function geocode(location) {
+function geocode(location, res2) {
   superagent
     .get("https://api.traveltimeapp.com/v4/geocoding/search")
     .query({ query: location })
@@ -91,25 +89,64 @@ function geocode(location) {
       result = JSON.parse(res.text);
       coords = result.features[0].geometry.coordinates;
       latlng = { lat: coords[1], lng: coords[0] };
-      getTimeMapAPI(latlng);
+      post_info.coordinates = latlng;
+      getTimeMapAPI(latlng, res2);
     });
 }
 
 function renderIndex(res, bool) {
   if (bool == true) {
     res.render("index", {
-      passworderror: true,
+      error: true,
     });
   } else {
     res.render("index", {
-      passworderror: false,
+      error: false,
     });
   }
 }
 
+function calculateGroundTransferSpeed(time, size) {
+  tb = size;
+  mn = time;
+
+  speed = (tb * 1024 * 1024) / (60 * mn);
+
+  return speed.toFixed(2);
+}
+
+function renderResult(res, geometry, post_info) {
+  let geo_string = JSON.stringify(geometry);
+  // console.log(geo_string);
+  res.render("result", {
+    bounds: geo_string,
+    location: post_info.location,
+    data_size: post_info.terabytes,
+    time: post_info.minutes,
+    int_speed: post_info.int_speed,
+    gt_speed: calculateGroundTransferSpeed(
+      post_info.minutes,
+      post_info.terabytes
+    ),
+    coordinates: JSON.stringify(post_info.coordinates),
+  });
+}
+
 app.get("/", function (req, res) {
   renderIndex(res, false);
-  geocode("Seattle, WA");
+});
+
+app.get("/result", function (req, res) {
+  renderResult(res, geometry, post_info);
+});
+
+app.post("/api", function (req, res) {
+  console.log(req.body);
+  post_info.location = req.body.location;
+  post_info.terabytes = req.body.data_size;
+  post_info.minutes = req.body.time;
+  post_info.int_speed = req.body.int_speed;
+  geocode(post_info.location, res);
 });
 
 const app_server = app.listen(app_port, () =>
